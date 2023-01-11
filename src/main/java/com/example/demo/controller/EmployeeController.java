62defb7ca677d965c78e696194f60aa7e072a563
gauravgambhir1 <gauravgambhir1992@gmail.com>
2023-01-11 16:14:49 +0530

    Initial Commit

---
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -0,0 +1,79 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.entity.Employee;
+import com.example.demo.service.EmployeeService;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService service;
+	
+	@GetMapping("/")
+	public String home(Model m)
+	{
+		List<Employee> emp=service.getAllEmp();
+		m.addAttribute("emp",emp);
+		return "index";
+	}
+	
+	@GetMapping("/addemp")
+	public String addEmpForm()
+	{
+		return "add_employee";
+	}
+
+	
+	
+	@PostMapping("/register")
+	public String empRegister(@ModelAttribute Employee e,HttpSession session)
+	{
+		System.out.println(e);
+		
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee details added successfully.");
+		return "redirect:/";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable int id,Model m)
+	{
+		
+		Employee e =service.getEmpById(id);
+		m.addAttribute("emp",e);
+		return "edit";
+	}
+	
+	@PostMapping("/update")
+	public String updateEmp(@ModelAttribute Employee e, HttpSession session)
+	{
+		service.addEmp(e);
+		session.setAttribute("msg", "Employee Data Updated Successfully.");
+		return "redirect:/";
+	}
+	
+	@GetMapping("/delete/{id}")
+	public String deleteEmp(@PathVariable int id,HttpSession session)
+	{
+		service.deleteEmp(id);
+		session.setAttribute("msg", "Employee Data Deleted Successfully.");
+		return "redirect:/";
+	}
+	
+}
